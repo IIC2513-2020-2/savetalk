5fed917959b335aeee13363b1aba33d4d11e9639
Sebastián Vicencio <sivicencio@gmail.com>
2020-10-08 04:28:34 -0300

    feat(users): add user model

---
+++ src/models/user.js
@@ -0,0 +1,38 @@
+
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('user', {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  }, {});
+  /* eslint-disable-next-line no-unused-vars */
+  user.associate = function associate(models) {
+    // associations can be defined here
+  };
+  return user;
+};
